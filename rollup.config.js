72183e451991d26c95ed4572aff301d270e39ac2
Christian Unigarro <cunigarrod@gmail.com>
2020-06-04 12:22:14 -0500

    Compile multiples components.

--- rollup.config.js
+++ rollup.config.js
@@ -3,42 +3,49 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import autoPreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
+const plugins = [
+	svelte({
+		// enable run-time checks when not in production
+		dev: !production,
+		// we'll extract any component CSS out into
+		// a separate file — better for performance
+		customElement: true,
+		preprocess: autoPreprocess()
+	}),
+
+	// If you have external dependencies installed from
+	// npm, you'll most likely need these plugins. In
+	// some cases you'll need additional configuration —
+	// consult the documentation for details:
+	// https://github.com/rollup/rollup-plugin-commonjs
+	resolve(),
+	commonjs(),
+
+	// Watch the `public` directory and refresh the
+	// browser on changes when not in production
+	!production && livereload('public'),
+
+	// If we're building for production (npm run build
+	// instead of npm run dev), minify
+	production && terser()
+];
+
+export default [{
+  input: 'src/components/Buttons.svelte',
+  output: {
+    file: 'public/buttons.js',
+    format: 'iife'
 	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve(),
-		commonjs(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	]
-};
+	plugins
+},{
+	input: 'src/components/Calculator.svelte',
+  output: {
+    file: 'public/calculator.js',
+    format: 'iife'
+	},
+	plugins
+}];
---
+++ src/components/Buttons.svelte
@@ -0,0 +1,25 @@
+<svelte:options tag="button-element" />
+
+<script>
+  export let name = 'world';
+  export let prop1 = 0;
+
+  function changeNumbers() {
+    prop1 += 1;
+  }
+</script>
+
+<style type="text/scss">
+  @import '../scss/buttons';
+</style>
+
+<h1>
+  {name}
+</h1>
+<button on:click={changeNumbers}>
+  Prueba
+</button>
+<div>
+  {prop1}
+</div>
+
